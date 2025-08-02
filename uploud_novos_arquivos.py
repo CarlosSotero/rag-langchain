97aa3bdbe49360173cad511898fb107c553d3d11
Carlos Sotero <100086042+CarlosSotero@users.noreply.github.com>
2025-08-02 18:08:18 -0300

    Atualizado para FASSI

--- uploud_novos_arquivos.py
+++ uploud_novos_arquivos.py
@@ -1,17 +1,16 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import Chroma
+from langchain.vectorstores import FAISS
 from langchain.embeddings import HuggingFaceEmbeddings
 import os
 
-# Função para processar os novos PDFs
-def processar_pdfs(pasta_pdf, pasta_base):
+def processar_pdfs(pasta_pdf, caminho_faiss):
     documentos = []
     for arquivo in os.listdir(pasta_pdf):
         if arquivo.endswith(".pdf"):
             loader = PyPDFLoader(os.path.join(pasta_pdf, arquivo))
             documentos.extend(loader.load())
-    
+
     splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=500, add_start_index=True)
     chunks = splitter.split_documents(documentos)
 
@@ -20,9 +19,11 @@ def processar_pdfs(pasta_pdf, pasta_base):
         encode_kwargs={'normalize_embeddings': True}
     )
 
-    # Carrega o banco existente e adiciona os novos documentos
-    db = Chroma(persist_directory=pasta_base, embedding_function=embeddings)
-    db.add_documents(chunks)
-    db.persist()
+    if os.path.exists(caminho_faiss):
+        db = FAISS.load_local(caminho_faiss, embeddings, allow_dangerous_deserialization=True)
+        db.add_documents(chunks)
+    else:
+        db = FAISS.from_documents(chunks, embeddings)
 
+    db.save_local(caminho_faiss)
     return len(chunks)
